a520ed9d02e89c7d719924836a06db1f2775fdda
unknown <darioguerraaragon@gmail.com>
2022-11-07 15:55:47 -0800

    trabajo con javascript casi terminado solo faltan ajustes

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
  const d = document
  let en = d.querySelector("#en")
  let es = d.querySelector("#es")
- let en_ = ""
- let es_ = ""
+ let btn_en = d.querySelector("#btn-en")
+ let en_text = d.querySelector("#en-text")
+ 
 
-  const options = {
+ en.addEventListener("keyup" , e =>{
+	en_text.innerHTML = en.value
+	
+	const options = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+			'X-RapidAPI-Key': '9c7658e5e3msh6211ef11f5e0a20p14924djsnc7b1bf1cb3e2',
+			'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
+		},
+		body: `{"q": "${en_text.textContent}","source":"en","target":"es"}`
+	};
+  
+	   fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
+		.then(response => response.json())
+		.then(response => {
+			// console.log(response)
+	  
+			arr = {...response}
+			let res = arr.data.translations.translatedText
+			// console.log(res)
+			es.innerHTML = res
+		})
+		.catch(err => console.error(err));
+ })
+
+
+  /*const options = {
   	method: 'POST',
   	headers: {
   		'content-type': 'application/json',
   		'X-RapidAPI-Key': '9c7658e5e3msh6211ef11f5e0a20p14924djsnc7b1bf1cb3e2',
   		'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
   	},
-  	body: `{"q": "hello everyone my name is dario and i wanna be your friend","source":"en","target":"es"}`
+  	body: `{"q": "hello","source":"en","target":"es"}`
   };
 
      fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
@@ -23,12 +51,4 @@
   		let res = arr.data.translations.translatedText
   		console.log(res)
   	})
-  	.catch(err => console.error(err));
-
-en.addEventListener("keyup" , e =>{
-	en_.innerHTML = en.value
-	
-	console.log(en_)
-})
-
-
+  	.catch(err => console.error(err));*/
