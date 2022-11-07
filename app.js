53f58996cc6941b67b335d6f76a7f29a4fe50a03
unknown <darioguerraaragon@gmail.com>
2022-11-07 14:53:04 -0800

    termine la maquetacion

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-// const d = document
-// let en = d.querySelector("#en")
-// let es = d.querySelector("#es")
+ const d = document
+ let en = d.querySelector("#en")
+ let es = d.querySelector("#es")
+ let en_ = ""
+ let es_ = ""
 
-// const options = {
-// 	method: 'POST',
-// 	headers: {
-// 		'content-type': 'application/json',
-// 		'X-RapidAPI-Key': '9c7658e5e3msh6211ef11f5e0a20p14924djsnc7b1bf1cb3e2',
-// 		'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
-// 	},
-// 	body: `{"q": "hello everyone my name is dario and i wanna be your friend","source":"en","target":"es"}`
-// };
+  const options = {
+  	method: 'POST',
+  	headers: {
+  		'content-type': 'application/json',
+  		'X-RapidAPI-Key': '9c7658e5e3msh6211ef11f5e0a20p14924djsnc7b1bf1cb3e2',
+  		'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
+  	},
+  	body: `{"q": "hello everyone my name is dario and i wanna be your friend","source":"en","target":"es"}`
+  };
 
-// fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
-// 	.then(response => response.json())
-// 	.then(response => {
-// 		// console.log(response)
+     fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
+  	.then(response => response.json())
+  	.then(response => {
+  		console.log(response)
 	
-// 		arr = {...response}
-// 		let res = arr.data.translations.translatedText
-// 		console.log(res)
-// 	})
-// 	.catch(err => console.error(err));
+  		arr = {...response}
+  		let res = arr.data.translations.translatedText
+  		console.log(res)
+  	})
+  	.catch(err => console.error(err));
+
+en.addEventListener("keyup" , e =>{
+	en_.innerHTML = en.value
+	
+	console.log(en_)
+})
+
+
