d3bc8df5a3c49ee99f6fb571041da10a1a174910
unknown <darioguerraaragon@gmail.com>
2022-11-09 13:41:58 -0800

    teniendo problemas

--- app.js
+++ app.js
@@ -3,9 +3,37 @@
  let es = d.querySelector("#es")
  let btn_en = d.querySelector("#btn-en")
  let en_text = d.querySelector("#en-text")
+
+//  variables para cambiar el idioma
+
+// rama para cambiar el idioma 
+const change = d.querySelector("#change")
+let espanol = "es"
+let ingles = "en"
  
 
- en.addEventListener("keyup" , e =>{
+en.addEventListener("keyup" , e =>{
+	let es_title = d.querySelector(".es_title")
+	let en_title = d.querySelector(".en_title")
+
+	/*change.addEventListener("click" , e =>{
+		
+		if(es_title.textContent === "Spanish"){
+			es_title.textContent = "English"
+			en_title.textContent = "Spanish"
+	
+			espanol.textContent = "en"
+			ingles.textContent = "es"
+			
+		}else{
+			es_title.textContent = "English"
+			en_title.textContent = "Spanish"
+	
+			espanol.textContent = "es"
+			ingles.textContent = "en"
+		}
+	})*/
+
 	en_text.innerHTML = en.value
 	
 	const options = {
@@ -21,7 +49,7 @@
 	   fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
 		.then(response => response.json())
 		.then(response => {
-			// console.log(response)
+			console.log(response)
 	  
 			arr = {...response}
 			let res = arr.data.translations.translatedText
@@ -29,26 +57,5 @@
 			es.innerHTML = res
 		})
 		.catch(err => console.error(err));
- })
 
-
-  /*const options = {
-  	method: 'POST',
-  	headers: {
-  		'content-type': 'application/json',
-  		'X-RapidAPI-Key': '9c7658e5e3msh6211ef11f5e0a20p14924djsnc7b1bf1cb3e2',
-  		'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
-  	},
-  	body: `{"q": "hello","source":"en","target":"es"}`
-  };
-
-     fetch('https://deep-translate1.p.rapidapi.com/language/translate/v2', options)
-  	.then(response => response.json())
-  	.then(response => {
-  		console.log(response)
-	
-  		arr = {...response}
-  		let res = arr.data.translations.translatedText
-  		console.log(res)
-  	})
-  	.catch(err => console.error(err));*/
+	})
